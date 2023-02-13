41e4522be0da2bbef0abba5c624e12cc49267c34
alphaQ054002 <nkhan054002@gmail.com>
2023-02-13 13:06:22 +0530

    smrt contract written, yet to be tested

--- my-app/backend/contracts/InsureAssist.sol
+++ my-app/backend/contracts/InsureAssist.sol
@@ -1,48 +1,32 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./Tranche.sol";
-import "./ITranche.sol";
+pragma solidity ^0.8.1;
 
-//this contract will hold the assets...?
-
-contract InsureAssist {
-    uint256 public immutable S;
-    uint256 public immutable T1;
-    uint256 public immutable T2;
-    uint256 public immutable T3;
-    IERC20 insuredAsset;//is this correct?
-
-    address x;//Protocol X
-    address y;//Protocol Y
-
-    constructor(uint256 _S, uint256 _T1, uint256 _T2, uint256 _T3, address _insuredAsset, address _x, address _y) {
-        S = _S;
-        T1 = _T1;
-        T2 = _T2;
-        T3 = _T3;
-        insuredAsset = IERC20(_insuredAsset);
-        x = _x;
-        y = _y;
+contract Proxy {
+    // Code position in storage is keccak256("PROXIABLE") = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
+    constructor(bytes memory constructData, address contractLogic) {
+        // save the code address
+        assembly { // solium-disable-line
+            sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, contractLogic)
+        }
+        (bool success, bytes memory result ) = contractLogic.delegatecall(constructData); // solium-disable-line
+        require(success, "Construction failed");
     }
 
-    function mintTranches (uint256 amount_c) public {
-        //mint tranche A
-        //mint tranche B
-        require(insuredAsset.transferFrom(tx.origin, address(this), amount_c));
-
-        ITranche(A).mint(tx.origin, amount_c / 2);//time becomes important, because then anyone can mint the tranches and invest
-        ITranche(B).mint(tx.origin, amount_c / 2);
-    };
-
-    function invest () public virtual override {
-        //invest in the asset
-        //transfer the asset to the split contract
-        //transfer the incentive to the split contract
-        uint256 balance_c = insuredAsset.balanceOf(address(this));
-        require(balance_c > 0, "insureAssist: no assets to invest");
-
-        //add Methods to Invest in protocols X and Y, to invest balance_c/2 in each protocol
-    };
+    fallback() external payable {
+        assembly { // solium-disable-line
+            let contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
+            calldatacopy(0x0, 0x0, calldatasize())
+            let success := delegatecall(sub(gas(), 10000), contractLogic, 0x0, calldatasize(), 0, 0)
+            let retSz := returndatasize()
+            returndatacopy(0, 0, retSz)
+            switch success
+            case 0 {
+                revert(0, retSz)
+            }
+            default {
+                return(0, retSz)
+            }
+        }
+    }
 }
