99deec5b2f1e5a5a1fb4e43e1fbbfe2f729734e0
alphaQ054002 <nkhan054002@gmail.com>
2023-03-02 19:03:52 +0530

    assist contract is 0x00 updated

--- my-app/backend/contracts/InsureAssist.sol
+++ my-app/backend/contracts/InsureAssist.sol
@@ -16,13 +16,12 @@ contract Proxy {
         require(success, "Construction failed");
     }
 
-    function getContractLogic () public returns (address){
-        address _implementation;
-        assembly { // solium-disable-line
-            _implementation := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
-        }
-        return _implementation;
-    }
+    // function getContractLogic () public returns (address){
+        // assembly { // solium-disable-line
+            // _implementation := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
+        // }
+    // }
+    //check the above thing lataer
 
     fallback() external payable {
         assembly { // solium-disable-line
